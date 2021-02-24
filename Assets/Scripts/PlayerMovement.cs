397205888908f9990b8b52fc5d091c925882daf5
Grégoire Lebas <gregoire.lebas@gmail.com>
2021-02-24 11:52:17 +0100

    Created a script for player to move around.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private float speed = 12.0f;
+	[SerializeField] private float gravity = -9.81f;
+
+	private CharacterController controller = null;
+
+	private Vector3 velocity = Vector3.zero;
+
+	private void Awake()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+
+	private void Update()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		Vector3 move = transform.right * x + transform.forward * z;
+
+		controller.Move(move * speed * Time.deltaTime);
+
+		velocity.y += gravity * Time.deltaTime;
+
+		controller.Move(velocity * Time.deltaTime);
+	}
+}
