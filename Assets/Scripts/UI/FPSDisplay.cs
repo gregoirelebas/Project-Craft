6aba532accb80ab7de540fa7ace565c6bf80f0c0
Grégoire Lebas <gregoire.lebas@gmail.com>
2021-03-16 13:26:45 +0100

    Implemented a fps display in canvas.

---
+++ Assets/Scripts/UI/FPSDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FPSDisplay : MonoBehaviour
+{
+	private TextMeshProUGUI fpsText = null;
+
+	private int frameCount = 0;
+	private float totalFPS = 0.0f;
+
+	private void Awake()
+	{
+		fpsText = GetComponent<TextMeshProUGUI>();
+
+		Application.targetFrameRate = 120;
+	}
+
+	private void Update()
+	{
+		totalFPS += 1 / Time.unscaledDeltaTime;
+		frameCount++;
+
+		fpsText.text = (totalFPS / frameCount).ToString("F0");
+	}
+}
